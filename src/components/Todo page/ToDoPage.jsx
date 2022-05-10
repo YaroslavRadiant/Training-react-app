db154a77cbf29a6f42ed1635fa8c76b0fddfc0cc
Yaroslav <qwertyarik92@gmail.com>
2022-05-10 18:07:07 +0300

    added filters

--- src/components/Todo page/ToDoPage.jsx
+++ src/components/Todo page/ToDoPage.jsx
@@ -1,12 +1,15 @@
-import { useState, useContext, React } from 'react';
-import './toDo.css';
-import ToDoItem from './toDoItem/ToDoItem';
-import ToDoContext from '../../context/context';
+import { useState, useContext, React } from "react";
+import "./toDo.css";
+import ToDoItem from "./toDoItem/ToDoItem";
+import ToDoContext from "../../context/context";
 
 export default function ToDoPage() {
-  const { toDoList, addToDoItem } = useContext(ToDoContext);
-  const [nameInput, setNameInput] = useState('');
-  const [moreInfoInput, setMoreInfoInput] = useState('');
+  const { toDoList, addToDoItem, searchByName, searchByMoreInfo } =
+    useContext(ToDoContext);
+  const [nameInput, setNameInput] = useState("");
+  const [moreInfoInput, setMoreInfoInput] = useState("");
+  const [nameFilterInput, setNameFilterInput] = useState("");
+  const [moreInfoFilterInput, setMoreInfoFilterInput] = useState("");
 
   function settingNameInput(event) {
     setNameInput(event.target.value);
@@ -16,28 +19,57 @@ export default function ToDoPage() {
     setMoreInfoInput(event.target.value);
   }
 
+  function settingNameFilterInput(event) {
+    setNameFilterInput(event.target.value);
+  }
+
+  function settingMoreInfoFilterInput(event) {
+    setMoreInfoFilterInput(event.target.value);
+  }
+
   return (
     <div className="page-wrapper">
       <h1>ToDoPage</h1>
       <div className="input-area">
-        <p>What to do</p>
-        <p>{nameInput}</p>
-        <input value={nameInput} onChange={settingNameInput} />
-        <p>More information</p>
-        <p>{moreInfoInput}</p>
-        <input value={moreInfoInput} onChange={settingMoreInfoInput} />
-        <button onClick={()=>addToDoItem({nameInput, moreInfoInput})}>Add new todo</button>
         <div>
-          {toDoList?.map((el) => [
-            <ToDoItem
-              toDoName={el.todoName}
-              toDoMoreInfo={el.moreInfo}
-              checked={el.isDone}
-              id={el.id}
-              key={el.id}
-            ></ToDoItem>,
-          ])}
+          <p>What to do</p>
+          <p>{nameInput}</p>
+          <input value={nameInput} onChange={settingNameInput} />
+          <p>More information</p>
+          <p>{moreInfoInput}</p>
+          <input value={moreInfoInput} onChange={settingMoreInfoInput} />
+          <button onClick={() => addToDoItem({ nameInput, moreInfoInput })}>
+            Add new todo
+          </button>
         </div>
+        <div>
+          <p>Name filter</p>
+          <p>{nameFilterInput}</p>
+          <input
+            value={nameFilterInput}
+            onChange={settingNameFilterInput}
+            onKeyUp={() => searchByName(nameFilterInput)}
+          />
+
+          <p>Mor info filter</p>
+          <p>{moreInfoFilterInput}</p>
+          <input
+            value={moreInfoFilterInput}
+            onChange={settingMoreInfoFilterInput}
+            onKeyUp={() => searchByMoreInfo(moreInfoFilterInput)}
+          />
+        </div>
+      </div>
+      <div>
+        {toDoList?.map((el) => [
+          <ToDoItem
+            toDoName={el.todoName}
+            toDoMoreInfo={el.moreInfo}
+            checked={el.isDone}
+            id={el.id}
+            key={el.id}
+          ></ToDoItem>,
+        ])}
       </div>
     </div>
   );
--- src/context/context.js
+++ src/context/context.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const defaultContextState = [
   {
-    todoName: 'Read a book',
-    moreInfo: '50 pages every day',
+    todoName: "Read a book",
+    moreInfo: "50 pages every day",
     isDone: false,
     id: 1,
   },
-  { todoName: 'Bye food', moreInfo: 'Eggs and milk', isDone: false, id: 2 },
-  { todoName: 'Go sleep', moreInfo: 'All night', isDone: true, id: 3 },
+  { todoName: "Bye food", moreInfo: "Eggs and milk", isDone: false, id: 2 },
+  { todoName: "Go sleep", moreInfo: "All night", isDone: true, id: 3 },
 ];
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case 'addToDo': {
+    case "addToDo": {
       return [
         ...state,
         {
@@ -24,10 +24,10 @@ export const reducer = (state, action) => {
         },
       ];
     }
-    case 'deleteToDo': {
+    case "deleteToDo": {
       return state.filter((el) => el.id !== action.id);
     }
-    case 'toggleToDo': {
+    case "toggleToDo": {
       return state.map((el) => {
         if (el.id === action.id) {
           return { ...el, isDone: !el.isDone };
@@ -35,6 +35,56 @@ export const reducer = (state, action) => {
         return el;
       });
     }
+    case "filterName": {
+      // if (action.filterNameInputValue.length === 0) {
+      //   console.log(state);
+      //   return state;
+      // }
+      // return state.filter((el) => {
+      //   let newRG = new RegExp(action.filterNameInputValue, "i");
+      //   if (newRG.test(el.todoName)) {
+      //     return true;
+      //   } else return false;
+      // });
+      console.log(
+        state.filter((el) => {
+          // debugger
+          if (action.filterNameInputValue === "") {
+            return state;
+          } else if (el.todoName.includes(action.filterNameInputValue)) {
+            return true;
+          }
+          return false;
+        })
+      );
+      // break;
+    }
+    // eslint-disable-next-line no-fallthrough
+    case "filterByMoreInfo": {
+      // if (action.filterMoreInfoValue.length === 0) {
+      //   console.log(state);
+      //   return state;
+      // }
+      // return state.filter((el) => {
+      //   let newRG = new RegExp(action.filterMoreInfoValue, "i");
+      //   if (newRG.test(el.moreInfo)) {
+      //     return true;
+      //   } else return false;
+      // });
+      console.log("IT WORKS");
+      console.log(
+        state.filter((el) => {
+          if (action.filterMoreInfoValue === "") {
+            return state;
+          } else if (el.moreInfo.includes(action.filterMoreInfoValue)) {
+            return true;
+          }
+          return false;
+        })
+      );
+      // break;
+    }
+    // eslint-disable-next-line no-fallthrough
     default:
       return state;
   }
@@ -47,13 +97,19 @@ export const ToDoProvider = ({ children }) => {
   const value = {
     toDoList: state,
     addToDoItem: (newToDo) => {
-      dispatch({ type: 'addToDo', newToDo });
+      dispatch({ type: "addToDo", newToDo });
     },
     deleteToDoItem: (id) => {
-      dispatch({ type: 'deleteToDo', id });
+      dispatch({ type: "deleteToDo", id });
     },
     toggleToDoItem: (id) => {
-      dispatch({ type: 'toggleToDo', id });
+      dispatch({ type: "toggleToDo", id });
+    },
+    searchByName: (filterNameInputValue) => {
+      dispatch({ type: "filterName", filterNameInputValue });
+    },
+    searchByMoreInfo: (filterMoreInfoValue) => {
+      dispatch({ type: "filterByMoreInfo", filterMoreInfoValue });
     },
   };
   return <ToDoContext.Provider value={value}>{children}</ToDoContext.Provider>;
