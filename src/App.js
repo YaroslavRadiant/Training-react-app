3d619c7d5410ac5294a472b819764175343d7bae
extypi <extypi@anthillagency.com>
2022-07-14 15:24:38 +0300

    test new branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import ElsePage from "./components/pages/Else-page/ElsePage";
-import NotFound from "./components/pages/Not-found/NotFound";
-import Homepage from "./components/pages/Homepage/Homepage";
-import ToDoPage from "./components/pages/Todo-page/ToDoPage";
-import Layout from "./components/layout/Layout";
+import ElsePage from './components/pages/Else-page/ElsePage';
+import NotFound from './components/pages/Not-found/NotFound';
+import Homepage from './components/pages/Homepage/Homepage';
+import ToDoPage from './components/pages/Todo-page/ToDoPage';
+import Layout from './components/layout/Layout';
 
-import { Route, Routes } from "react-router-dom";
-import CurrentPostPage from "./components/pages/Current-post-page/CurrentPostPage";
-import WeatherPage from "./components/pages/Weather-page/WeatherPage";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "./store/index";
-
+import { Route, Routes } from 'react-router-dom';
+import CurrentPostPage from './components/pages/Current-post-page/CurrentPostPage';
+import WeatherPage from './components/pages/Weather-page/WeatherPage';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/index';
+//new-branch
 export default function App() {
   return (
     <Provider store={store}>
